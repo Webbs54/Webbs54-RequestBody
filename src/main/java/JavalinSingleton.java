266035b2448b3ef5e100df6dde74c6067cc1321c
Eduardo Tocco <eduardo.tocco1@gmail.com>
2023-02-23 20:45:33 +0000

    Completed Lab Request Body

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -12,7 +12,7 @@ public class JavalinSingleton {
 
     public static Javalin getInstance(){
         Javalin app = Javalin.create();
-        ObjectMapper om = new ObjectMapper();
+        
         
         /**
          * problem1: retrieve the song object from the request body...
@@ -21,7 +21,17 @@ public class JavalinSingleton {
          * Note: Please refer to the "RequestBody.MD" file for more assistance.
          */
         app.post("/echo", ctx -> {
+            //retrieve the json string from the request body
+            String jsonString = ctx.body();
+
+            //utilize jackson to convert the json string to a user object
+            ObjectMapper om = new ObjectMapper();
+            Song echo = om.readValue(jsonString, Song.class);
             
+            //now we can use the 'user' response body as a Java object in whatever way we see fit.
+                
+            //eg: return the user as the response body, but also have Javalin convert it to JSON  
+            ctx.json(echo);  
             //implement logic here
                 
         });
@@ -34,8 +44,19 @@ public class JavalinSingleton {
          * Note: Please refer to the "RequestBody.MD" file for more assistance.
          */
         app.post("/changeartisttobeatles", ctx -> {
+            //retrieve the json string from the request body
+            String jsonString=ctx.body();
 
-            //implement logic here
+            //utilize jackson to convert the json string to a user object
+            ObjectMapper om=new ObjectMapper();
+            Song song = om.readValue(jsonString, Song.class);
+
+            //change the last name
+            String b = "Beatles";
+            song.setArtistName(b);
+
+            //generate an HTTP response with the user object in the response body as a JSON.
+            ctx.json(song);
                
         });
 
